0399d5a2a013e9da97bbe413b268ba664a82aba5
Kelly Lee <amorfati38@gmail.com>
2022-03-24 19:14:10 -0600

    added empty routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+// GET /api/users
+router.get('/', (req, res) => {
+
+});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+  
+});
+
+// POST /api/users creat a user
+router.post('/', (req, res) => {
+  
+});
+
+// login
+router.post('/login', (req, res) => {
+  
+});
+
+// logout
+router.post('/logout', (req, res) => {
+
+});
+
+// PUT /api/users/1 update user information
+router.put('/:id', (req, res) => {
+
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+// get all candies
+router.get('/', (req, res) => {
+    
+});
+
+// login
+router.get('/login', (req, res) => {
+
+});
+
+// get one candy
+router.get('/post/:id', (req, res) => {
+
+});
+
+module.exports = router;
