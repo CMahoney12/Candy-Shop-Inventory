1a4e2d5dcb200f77bf7fd0d6a19553efde2b4685
Otterpop7 <Jordanotterson7@gmail.com>
2022-03-29 19:38:11 -0600

    Updated Front End
    we need to fix navbar probably

--- seeds/candy-seeds.js
+++ seeds/candy-seeds.js
@@ -69,6 +69,86 @@ const candyData = [{
         title: "Nestle Crunch",
         price: 1.50,
         filename: 'Nestle Crunch.webp'
+    },
+    {
+        title: 'Butterfinger',
+        price: 1.65,
+        filename: 'Butterfinger.jpg'
+    },
+    {
+        title: 'Chunky',
+        price: 2.40,
+        filename: 'Chunky Bar.jpg'
+    },
+    {
+        title: 'Mounds',
+        price: 1.99,
+        filename: 'Mounds.jpg'
+    },
+    {
+        title: 'Bounty',
+        price: 1.50,
+        filename: 'Bounty-Bar.jpg'
+    },
+    {
+        title: 'Nestle Crunch',
+        price: 2.20,
+        filename: 'Nestle Crunch.webp'
+    },
+    {
+        title: '100 Grand',
+        price: 1.75,
+        filename: '100 Grand.webp'
+    },
+    {
+        title: '5th Avenue',
+        price: 2.99,
+        filename: '5th Avenue.jpg'
+    },
+    {
+        title: 'Mars',
+        price: 1.65,
+        filename: 'Mars.jpg'
+    },
+    {
+        title: "Reese's Fast Break",
+        price: 1.99,
+        filename: "Reese's Fast Break.jpg"
+    },
+    {
+        title: 'Milky Way',
+        price: 1.50,
+        filename: 'Milky Way.png'
+    },
+    {
+        title: 'Oreo Chocolate Candy Bar',
+        price: 1.75,
+        filename: 'Oreo Chocolate Candy Bar.jpg'
+    },
+    {
+        title: "Symphony",
+        price: 2.99,
+        filename: "Symphony.jpg"
+    },
+    {
+        title: "Mr. Goodbar",
+        price: 1.50,
+        filename: 'Mr. Goodbar.jpg'
+    },
+    {
+        title: "Hershey's Cookies 'n' Creme",
+        price: 1.50,
+        filename: "Hershey's Cookies 'n' Creme.jpg"
+    },
+    {
+        title: "3 Musketeers",
+        price: 1.50,
+        filename: '3 Musketeers.webp'
+    },
+    {
+        title: "Hershey's Milk Chocolate",
+        price: 1.50,
+        filename: "Hershey's Milk Chocolate.jpg"
     }
 ]
 
